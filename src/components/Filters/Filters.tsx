e7a0bf4cc69a0719752a6dd9d9a5fd3437c078fa
Uche Mukolo <uchek7@gmail.com>
2024-02-16 16:27:17 +0000

    memoize selectedOptions

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,5 +1,5 @@
-import React, { ChangeEvent } from "react";
-import { Option } from '../data/productDataTypes' // Importing types from productDataTypes
+import React, { useMemo } from "react";
+import { Option } from '../data/productDataTypes';
 import './Filters.css';
 
 type Category = {
@@ -15,56 +15,56 @@ type FiltersProps = {
 }
 
 const Filters: React.FC<FiltersProps> = ({ data, onChange, selectedOptions }) => {
-  const handleOptionChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    // Toggle option selection in the selectedOptions set
-    if (selectedOptions.has(value)) {
-      selectedOptions.delete(value);
+  // Memoize the selected options to prevent unnecessary re-renders
+  const selectedOptionsArray = useMemo(() => Array.from(selectedOptions), [selectedOptions]);
+
+  const handleOptionChange = (value: string) => {
+    const updatedOptions = new Set(selectedOptionsArray);
+    if (updatedOptions.has(value)) {
+      updatedOptions.delete(value);
     } else {
-      selectedOptions.add(value);
+      updatedOptions.add(value);
     }
-    onChange([...selectedOptions]);
+    onChange(Array.from(updatedOptions));
   };
 
   return (
     <div>
-      {data.map((category, index) => {
-        return (
-          <div key={index}>
-            {!category.ungrouped && <p>{category.label}</p>}
-            {category.options.map((option, idx) => (
-              <div key={idx} className="label-text">
-                <label>
-                  <input
-                    type="checkbox"
-                    value={option.value}
-                    checked={[...selectedOptions].includes(option.value)}
-                    onChange={handleOptionChange}
-                  />
-                  {option.label}
-                </label>
-                {option.options && (
-                  <div style={{ marginLeft: 20 }}>
-                    {option.options.map((subOption, subIdx) => (
-                      <div key={subIdx} className="label-text">
-                        <label>
-                          <input
-                            type="checkbox"
-                            value={subOption.value}
-                            checked={[...selectedOptions].includes(subOption.value)}
-                            onChange={handleOptionChange}
-                          />
-                          {subOption.label}
-                        </label>
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
-        )
-      })}
+      {data.map((category, index) => (
+        <div key={index}>
+          {!category.ungrouped && <p>{category.label}</p>}
+          {category.options.map((option, idx) => (
+            <div key={idx} className="label-text">
+              <label>
+                <input
+                  type="checkbox"
+                  value={option.value}
+                  checked={selectedOptions.has(option.value)}
+                  onChange={() => handleOptionChange(option.value)}
+                />
+                {option.label}
+              </label>
+              {option.options && (
+                <div style={{ marginLeft: 20 }}>
+                  {option.options.map((subOption, subIdx) => (
+                    <div key={subIdx} className="label-text">
+                      <label>
+                        <input
+                          type="checkbox"
+                          value={subOption.value}
+                          checked={selectedOptions.has(subOption.value)}
+                          onChange={() => handleOptionChange(subOption.value)}
+                        />
+                        {subOption.label}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
